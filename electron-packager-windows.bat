91b582cf4400ff225a33bae90eb2f7ebcd1b8710
Atle Magnussen <atlemagnussen@gmail.com>
2017-02-23 14:50:37 +0100

    added packer command

---
+++ electron-packager-windows.bat
@@ -0,0 +1,1 @@
+electron-packager . --overwrite --asar=true --platform=win32 --arch=x64 --icon=assets/icons/win/icon.ico --prune=true --out=release-builds --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName=\"Test\"
