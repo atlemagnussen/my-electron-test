4bd65d2d7b2e4df6d4fbe3c1851ba200c5e3acd8
Atle Magnussen <atlemagnussen@gmail.com>
2017-02-27 09:00:51 +0100

    Did some stuff

--- app/components/home/home.component.js
+++ app/components/home/home.component.js
@@ -5,8 +5,22 @@ angular.module('homeModule').component('homeView', {
             this.hello = "Hello Angular";
             this.url = "https://docs.angularjs.org/guide/component";
 
-            this.selection = 'home';
-            this.items = ['home', 'settings', 'options', 'default']
+            this.selectionIndex = 0;
+            this.selection = '0';
+            this.items = ['0', '1', '2', '3', '4']
+            this.next = function() {
+                this.selectionIndex++;
+                if (this.selectionIndex > (this.items.length-1))
+                    this.selectionIndex--;
+                this.selection = this.selectionIndex.toString();
+            }
+
+            this.previous = function() {
+                this.selectionIndex--;
+                if (this.selectionIndex < 0)
+                    this.selectionIndex++;
+                this.selection = this.selectionIndex.toString();
+            }
             this.setSelection = function(item) {
                 this.selection = item;
             }
--- main.js
+++ main.js
@@ -1,29 +1,22 @@
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Keep a global reference of the window object, if you don't, the window will be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
   // mainWindow.setMenu(null);
-
-  // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'app/index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
@@ -35,9 +28,6 @@ function createWindow () {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
