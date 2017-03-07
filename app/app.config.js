ae0edd257c72ba4662f517fe93cd70304b3864c2
Atle Magnussen <atlemagnussen@gmail.com>
2017-03-08 00:13:58 +0100

    do some more

--- app/app.config.js
+++ app/app.config.js
@@ -14,4 +14,21 @@
         otherwise('/home');
         }
     ]);
+
+    angular.module('app').directive('nagPrism', [function() {
+    return {
+        restrict: 'A',
+        scope: {
+            source: '@'
+        },
+        link: function(scope, element, attrs) {
+            scope.$watch('source', function(v) {
+                if(v) {
+                    Prism.highlightElement(element.find("code")[0]);
+                }
+            });
+        },
+        template: "<code ng-bind='source'></code>"
+    };
+}]);
 })(window.angular);
--- app/components/home/home.component.js
+++ app/components/home/home.component.js
@@ -4,14 +4,16 @@ angular.module('homeModule').component('homeView', {
     controller: ['$routeParams', function HomeViewController($routeParams) {
             this.hello = "Hello Angular";
             this.url = "https://docs.angularjs.org/guide/component";
-
+            this.mainjs = new TextDecoder("utf-8").decode(mainJsContent);
+            this.packagejson = new TextDecoder("utf-8").decode(packageJsonContent);
             this.selectionIndex = 0;
             this.selection = '0';
-            this.items = ['0', '1', '2', '3', '4']
+            this.items = ['0', '1', '2', '3', '4', '5']
+
             this.next = function() {
                 this.selectionIndex++;
                 if (this.selectionIndex > (this.items.length-1))
-                    this.selectionIndex--;
+                    this.selectionIndex = 0;
                 this.selection = this.selectionIndex.toString();
             }
 
